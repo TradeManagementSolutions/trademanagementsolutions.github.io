f573aea8d63af937a3ba3348d40ff5323ca5c2d4
Ricardo Quintanar <lrq1012@gmail.com>
2020-07-01 15:46:56 -0500

    m

--- .history/JS/app_20200701154521.js
+++ .history/JS/app_20200701154521.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jul 3, 2020 18:00:00").getTime();
+var countDownDate = new Date("Ju 3, 2020 18:00:00").getTime();
 
 // Update the count down every 1 second
 var countdownfunction = setInterval(function() {
